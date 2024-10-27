24580e10bd7cd6c10e8e61efcbef5c001be4694b
Savio Resende <savio@savioresende.com>
2024-10-27 17:16:22 -0500

    Added under construction.

--- resources/views/livewire/library.blade.php
+++ resources/views/livewire/library.blade.php
@@ -1,5 +1,6 @@
 <div>
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        Under construction...
         {{--<div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
             <div class="flex-shrink-0">
                 <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
